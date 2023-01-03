6f2554104ebba8b468a9263044ca2927f45fa130
ZKX-0326 <relax90326@gmail.com>
2023-01-03 23:45:46 +0800

    alg111a

--- CNK/RCnk.py
+++ CNK/RCnk.py
@@ -1,9 +1,13 @@
 import random
 def RCNK(n, k):
     listrcnk = []
+    lista = []
+    for m in range(1, n+1):
+        lista.append(m)
     for j in range(0, k):
-        i = random.randrange(1, n+1)
+        i = random.choice(lista)
         listrcnk.append(i)
+        lista.remove(i)
     listrcnk.sort()
     if len(listrcnk) == k:
         print(listrcnk)
